9fa4427870a0ca1c32dbe9c881861e2237d9c625
sina rastgouyan <sina.rastgouyan@gmail.com>
2022-11-23 23:38:30 +0330

    file type ckeck implemented

--- functions.py
+++ functions.py
@@ -6,10 +6,10 @@ import sys
 def get_pdf_file():
     while True:
         pdf_file = input(r'Enter pdf file path please:''\n')
-        if not os.path.isfile(pdf_file):
-            print('Wrong path/file please check and enter again')
+        extract_file_name = pdf_file.split('\\')[-1]
+        if not os.path.isfile(pdf_file) or extract_file_name.split('.')[-1] != 'pdf':
+            print('Wrong path Or The file that has been entered is not pdf')
         else:
-            print('sounds good')
             break
 
 
