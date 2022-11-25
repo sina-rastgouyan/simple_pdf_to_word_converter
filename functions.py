10747094ab90090d291680ab10e751ca01168c47
sina rastgouyan <sina.rastgouyan@gmail.com>
2022-11-25 11:28:23 +0330

    Save the extracted data in a word file

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import os
 import PyPDF2
+import shutil
 
 
 # extract data from pdf file
@@ -7,7 +8,16 @@ def pdf_data_extract_extractor(pdf_file):
     with open(pdf_file, 'rb') as source_file:
         reader = PyPDF2.PdfFileReader(source_file)
         page = reader.getPage(0)
-        print(page.extractText())
+        text = page.extractText()
+    return text
+
+
+# transfer extracted data to destination file and turn to word
+def transformer(saved_location, text_extracted, converted_name):
+    extension_transform = converted_name.split('.')[0] + '.doc'
+    multi_line_string = """""" + text_extracted
+    with open(f"{saved_location}\\{extension_transform}", 'w') as writer:
+        writer.writelines(multi_line_string)
 
 
 # get path to save converted file
@@ -32,9 +42,8 @@ def get_pdf_file():
         else:
             break
     saved_files_location = get_destination_path_to_save()
-    pdf_data_extract_extractor(pdf_file)
-
-
+    text_extracted = pdf_data_extract_extractor(pdf_file)
+    transformer(saved_files_location, text_extracted, extract_file_name)
 
 
 get_pdf_file()
